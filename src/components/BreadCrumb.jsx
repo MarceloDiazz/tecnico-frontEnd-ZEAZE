0fd0c50bb9d623b16b5895b86d55ef03830ad920
MarceloDiazz <marcelodiazcp@gmail.com>
2024-10-07 20:28:22 -0300

    proceso: creacion BreadCrum component

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,17 @@
+
+const BreadCrumb = () => {
+    // flex
+    // 3 elementos 
+    // 1 : icono y texto
+    // icon arrow separador
+    // texto con fecha
+  return (
+    <section
+    className="container p-flex px-5"
+    >
+    
+    </section>
+  )
+}
+
+export default BreadCrumb
