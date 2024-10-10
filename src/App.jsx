a596307ebf97124b5b344189e60593cc7867dd97
MarceloDiazz <marcelodiazcp@gmail.com>
2024-10-10 19:17:08 -0300

    useEffect para obtener las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import BreadCrumb from "./components/BreadCrumb";
+import CardAlbum from "./components/CardAlbum";
 import Navbar from "./components/Navbar";
+
 import "./styles.css";
+import GridContainer from "./components/GridContainer";
+import { images } from "./utils/images";
+import FeaturedPhotographers from "./components/FeaturedPhotographers";
+import Footer from "./components/Footer";
+import { useEffect } from "react";
+import { setImages } from "./store/imageSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
-
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(setImages(images))
+  },[])
+ 
   return (
-    <div className="container border vh-100">
+    <div className="container-fluid p-0">
      <Navbar />
      <BreadCrumb />
+     <CardAlbum />
+     <GridContainer/>
+     <FeaturedPhotographers />
+     <Footer />
     </div>
   );
 }
