293ec51a368d6e72331802f578a6c79366d3eced
MarceloDiazz <marcelodiazcp@gmail.com>
2024-10-10 19:12:17 -0300

    json con informacion de las imágenes descargadas

---
+++ src/utils/images.js
@@ -0,0 +1,29 @@
+import image1 from "../assets/alex-konokh-6MKJbkZ0qNY-unsplash.jpg";
+import image2 from "../assets/amjith-s-VJGBwaB57Fg-unsplash.jpg";
+import image3 from "../assets/borna-bevanda-VwqecUsYKvs-unsplash.jpg";
+import image4 from "../assets/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg";
+import image5 from "../assets/diana-macesanu-FoY0-gwa9N4-unsplash.jpg";
+import image6 from "../assets/erik-jan-leusink---SDX4KWIbA-unsplash.jpg";
+import image7 from "../assets/hanny-naibaho--Go4DH2pZbc-unsplash.jpg";
+import image8 from "../assets/jonathan-fink-Sa1z1pEzjPI-unsplash.jpg";
+import image9 from "../assets/karlijn-prot-3fjyk4CLfvA-unsplash.jpg";
+import image10 from "../assets/ricky-kharawala-adK3Vu70DEQ-unsplash.jpg";
+import image11 from "../assets/sandy-millar-kKAaCeGf5wY-unsplash.jpg";
+import image12 from "../assets/smart-prf4NsEctEA-unsplash.jpg";
+import image13 from "../assets/sunira-moses-r149yvhlJ4Q-unsplash.jpg";
+
+export const images = [
+  { id: 1, url: image1 },
+  { id: 2, url: image2 },
+  { id: 3, url: image3 },
+  { id: 4, url: image4 },
+  { id: 5, url: image5 },
+  { id: 6, url: image6 },
+  { id: 7, url: image7 },
+  { id: 8, url: image8 },
+  { id: 9, url: image9 },
+  { id: 10, url: image10 },
+  { id: 11, url: image11 },
+  { id: 12, url: image12 },
+  { id: 13, url: image13 }
+];
