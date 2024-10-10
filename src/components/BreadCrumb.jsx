d6f3945ba657a1a501323096f769aa88549ed5bd
MarceloDiazz <marcelodiazcp@gmail.com>
2024-10-10 19:15:08 -0300

    pantalla: breadCrumb - documentada

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -1,17 +1,26 @@
-
+import iconAlbum from "../assets/icon-album.png"
+import "../css/bread-crum.css"
 const BreadCrumb = () => {
-    // flex
-    // 3 elementos 
-    // 1 : icono y texto
-    // icon arrow separador
-    // texto con fecha
-  return (
-    <section
-    className="container p-flex px-5"
-    >
-    
-    </section>
-  )
-}
-
-export default BreadCrumb
+    return (
+      <nav
+        style={{
+          "--bs-breadcrumb-divider": `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E")`, 
+        }}
+        aria-label="breadcrumb"
+        className=" breadcrumb-container"
+      >
+        <ol className="breadcrumb">
+          <li className="bread-item-custom-color breadcrumb-item d-flex gap-2 align-items-center">
+            <img src={iconAlbum} alt="icon-album" height={15} width={15} />
+            <a href="#">Álbumes</a>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            15/06/2024
+          </li>
+        </ol>
+      </nav>
+    );
+  };
+  
+  export default BreadCrumb;
+  
